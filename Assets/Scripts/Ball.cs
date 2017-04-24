ab5316ce1c4bc6712b427e2732c5e18aebcfb8ac
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-24 01:46:28 -0400

    Added ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 100.0f;
+
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+	}
+
+	float hitBall (Vector2 ballPos, Vector2 racketPos, float racketWidth) {
+		return (ballPos.x - racketPos.x) / racketWidth;
+	}
+
+	void onCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.name == "paddle_1") {
+			float x = hitBall (transform.position, other.transform.position, other.collider.bounds.size.x);
+
+			Vector2 dir = new Vector2(x, 1).normalized;
+
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+	}
+}

1f92d46cc258e1ff488e33bda26da6d29ddd6452
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-24 03:58:15 -0400

    Updated racket controller script

--- Assets/Scripts/RacketController.cs
+++ Assets/Scripts/RacketController.cs
@@ -5,20 +5,11 @@ using UnityEngine;
 public class RacketController : MonoBehaviour {
 
 	public float speed = 100.0f;
-	private Vector3 playerPosition;
 
-//	void FixedUpdate () {
-//		float horiz = Input.GetAxisRaw("Mouse X");
-//		Cursor.visible = false;
-//		GetComponent<Rigidbody2D>().velocity = Vector2.right * horiz * speed;
-//	}
-	void Start () {
-		playerPosition = gameObject.transform.position;
-	}
 	void FixedUpdate () {
+		float horiz = Input.GetAxisRaw("Mouse X");
 		Cursor.visible = false;
-		playerPosition.x += Input.GetAxisRaw ("Mouse X") * speed;
-
-		transform.position = playerPosition;
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * horiz * speed;
 	}
+
 }

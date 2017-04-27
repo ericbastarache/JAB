30d42e79ab3fa08cf0969b49003f54c27e2ae9fe
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-27 01:16:28 -0400

    Removed deprecated code

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,7 +7,6 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
 		SceneManager.LoadScene(name);
-		//Application.LoadLevel (name);
 	}
 
 	public void QuitRequest(){

07dc0e2854a3c81852ce7b8f5d3fd7d213a5f642
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-02 07:31:12 -0400

    Added a game manager script to manage game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public float lives = 3;
+	public float coinChance = 0.3;
+	public float growPaddleChance = 0.2;
+	public float extraLife = 0.05;
+	public float multipleBall = 0.1;
+	public float shrinkPowerDown = 0.3;
+	public float stickyPower = 0.4;
+	public float paddleUp = 0.25;
+	public float starPower = 0.02;
+	public float super = 0.01;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
